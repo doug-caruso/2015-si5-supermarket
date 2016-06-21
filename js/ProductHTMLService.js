6c6bf5658d438fed7a5b7500c59013df00e25f68
Douglas Caruso <doug-caruso@hotmail.com>
2016-06-21 12:16:26 -0300

    image added in the table

--- js/ProductHTMLService.js
+++ js/ProductHTMLService.js
@@ -1,16 +1,26 @@
 var ProductHTMLService = {
 
-displayAddedProduct: function(item) {
+    displayAddedProduct: function(item) {
         var tb = document.createElement('tbody');
         var tr = document.createElement('tr');
+        if(item.estoque<=10)
+            tr.className = "danger";
         var td = tr.appendChild(document.createElement('td'));
-        td.innerHTML = item.nome;
+        td.innerHTML = item.codigo;    
+        var td1 = tr.appendChild(document.createElement('td'));
+        td1.innerHTML = item.nome;
         var td2 = tr.appendChild(document.createElement('td'));
         td2.innerHTML = item.marca;
         var td3 = tr.appendChild(document.createElement('td'));
-        td3.innerHTML = item.preco;
+        td3.innerHTML = item.departamento;
         var td4 = tr.appendChild(document.createElement('td'));
-        td4.innerHTML = item.estoque;        
+        td4.innerHTML = item.preco;  
+        var td5 = tr.appendChild(document.createElement('td'));
+        td5.innerHTML = item.estoque;
+        var td6 = tr.appendChild(document.createElement('td'));
+        var img;
+        img = item.imagem.replace("C:\\fakepath\\", "C:\\Users\\suporte01\\Documents\\web\\tr\\TRABALHO\\img\\");
+        td6.innerHTML = ('<img src=\"'+img+'\"height="30" width="30" />');
         document.getElementById("tbl-products").appendChild(tr);
     }
 }
