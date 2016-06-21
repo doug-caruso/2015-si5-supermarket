742f6a514c3d821ec0028a3d6addd820841c5cdf
Douglas <Douglas>
2016-06-20 23:29:53 -0300

    delete from table

--- js/ProductController.js
+++ js/ProductController.js
@@ -6,7 +6,31 @@ var ProductController = {
     displayProdutos: function () {
         var produtos = ProductService.getProducts();
         produtos.forEach(ProductHTMLService.displayAddedProduct);
-    }
+    },
+
 };
 
 ProductController.init();
+
+function alertInnerHTML(e)
+{
+    console.log("ABC");
+    var json = JSON.parse(localStorage['list-products']);
+    console.log("ABC");
+    for (i=0;i<json.length;i++){
+            if (json[i].nome == this.innerHTML){
+                json.splice(i,1);
+            }
+    }
+    localStorage['list-products'] = JSON.stringify(json);
+
+}
+
+var theTbl = document.getElementById('tb');
+for(var i=0;i<theTbl.rows.length;i++)
+{
+    for(var j=0;j<theTbl.rows[i].cells.length;j++)
+    {
+        theTbl.rows[i].cells[j].onclick = alertInnerHTML;
+    }
+}
