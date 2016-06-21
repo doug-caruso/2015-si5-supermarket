86a2b4864f3f5b4b98783507fe8e693b2466c1c9
Douglas <Douglas>
2016-06-21 18:14:00 -0300

    alert and refresh button

--- js/ProductController.js
+++ js/ProductController.js
@@ -29,15 +29,14 @@ function alertInnerHTML(e)
     var json = JSON.parse(localStorage['list-products']);
     for (i=0;i<json.length;i++){
             if (json[i].codigo == this.innerHTML){
-                json.splice(i,1);
-                document.getElementById('tbl-products').innerHTML="<th>Código</th><th>Produto</th><th>Marca</th><th>Departamento</th><th>Preço</th><th>Estoque</th><th>Imagem</th>";
-                ProductController.displayProdutos();
+                var r;
+                r = confirm("Deseja excluir o produto "+json[i].nome+"?");
+                if(r==true)
+                    json.splice(i,1);
             }
     }
     localStorage['list-products'] = JSON.stringify(json);
-
 }
-
 var theTbl = document.getElementById('tb');
 for(var i=0;i<theTbl.rows.length;i++)
 {
