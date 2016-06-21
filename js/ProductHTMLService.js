dbfb05e56acd5d0e1072186c2892d6aca42b0f38
Douglas Caruso <doug-caruso@hotmail.com>
2016-06-21 09:14:10 -0300

    changed table

--- js/ProductHTMLService.js
+++ js/ProductHTMLService.js
@@ -2,14 +2,15 @@ var ProductHTMLService = {
 
 displayAddedProduct: function(item) {
         var tb = document.createElement('tbody');
-        var nome = item.nome;
         var tr = document.createElement('tr');
         var td = tr.appendChild(document.createElement('td'));
-        td.innerHTML = nome;
+        td.innerHTML = item.nome;
         var td2 = tr.appendChild(document.createElement('td'));
-        td2.innerHTML = item.preco;
+        td2.innerHTML = item.marca;
         var td3 = tr.appendChild(document.createElement('td'));
-        td3.innerHTML = item.estoque;        
+        td3.innerHTML = item.preco;
+        var td4 = tr.appendChild(document.createElement('td'));
+        td4.innerHTML = item.estoque;        
         document.getElementById("tbl-products").appendChild(tr);
     }
 }
